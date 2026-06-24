f7cc86237d68f5a5492b7ae90266843d49c93405
Jaskaran Singh <jaskaran.singh@ivalt.com>
2026-06-24 18:24:36 +0530

    feat: add axios instance with error interceptor

---
+++ lib/axios.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+api.interceptors.response.use(
+  response => response,
+  error => {
+    const message = error.response?.data?.error || error.message || 'Request failed';
+    return Promise.reject(new Error(message));
+  }
+);
+
+export default api;
